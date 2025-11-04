7a1430d4a6922383a8ceb91222784063d03a9dbb
AlexendrosDev <Alexendros@proton.me>
2025-11-04 03:53:49 +0100

    feat(region): registrar 4 CCAA adicionales en bootstrapRegions (Asturias, Navarra, Cantabria, Baleares)

--- src/regions/bootstrapRegions.js
+++ src/regions/bootstrapRegions.js
@@ -8,6 +8,10 @@ import IRPFGalicia2025 from '../regions/galicia/IRPFGalicia2025.js';
 import IRPFCastillaYLeon2025 from '../regions/castilla_y_leon/IRPFCastillaYLeon2025.js';
 import IRPFMurcia2025 from '../regions/murcia/IRPFMurcia2025.js';
 import IRPFAragon2025 from '../regions/aragon/IRPFAragon2025.js';
+import IRPFAsturias2025 from '../regions/asturias/IRPFAsturias2025.js';
+import IRPFNavarra2025 from '../regions/navarra/IRPFNavarra2025.js';
+import IRPFCantabria2025 from '../regions/cantabria/IRPFCantabria2025.js';
+import IRPFBaleares2025 from '../regions/baleares/IRPFBaleares2025.js';
 
 export function registerInitialRegions() {
   RegionManager.registerRegion('valencia', { name: 'Comunitat Valenciana', irpf: IRPFValencia2025 });
@@ -19,6 +23,10 @@ export function registerInitialRegions() {
   RegionManager.registerRegion('castilla_y_leon', { name: 'Castilla y León', irpf: IRPFCastillaYLeon2025 });
   RegionManager.registerRegion('murcia', { name: 'Región de Murcia', irpf: IRPFMurcia2025 });
   RegionManager.registerRegion('aragon', { name: 'Aragón', irpf: IRPFAragon2025 });
+  RegionManager.registerRegion('asturias', { name: 'Principado de Asturias', irpf: IRPFAsturias2025 });
+  RegionManager.registerRegion('navarra', { name: 'Navarra (foral)', irpf: IRPFNavarra2025 });
+  RegionManager.registerRegion('cantabria', { name: 'Cantabria', irpf: IRPFCantabria2025 });
+  RegionManager.registerRegion('baleares', { name: 'Illes Balears', irpf: IRPFBaleares2025 });
 }
 
 export default registerInitialRegions;
